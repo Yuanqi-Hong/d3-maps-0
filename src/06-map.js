bd8678e65002ac7fad9ac33b211ebc1f6beaffe6
Yuanqi (Edward) Hong <39491376+Yuanqi-Hong@users.noreply.github.com>
2018-10-10 14:54:11 -0400

    updated d3-maps-0

--- src/06-map.js
+++ src/06-map.js
@@ -1,11 +1,9 @@
 import * as d3 from 'd3'
 import * as topojson from 'topojson'
 
-let margin = { top: 0, left: 0, right: 0, bottom: 0 }
-
-let height = 600 - margin.top - margin.bottom
-
-let width = 900 - margin.left - margin.right
+let margin = { top: 0, left: 30, right: 0, bottom: 20 }
+let height = 610 - margin.top - margin.bottom
+let width = 940 - margin.left - margin.right
 
 let svg = d3
   .select('#chart-6')
@@ -14,11 +12,11 @@ let svg = d3
   .attr('width', width + margin.left + margin.right)
   .style('background', 'black')
   .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+  .attr('transform', `translate(${margin.left},${margin.top})`)
 
 let projection = d3.geoAlbersUsa()
 
-// out geoPath needs a PROJECTION variable
+// out geoPath needs a PROJECTION letiable
 let path = d3.geoPath().projection(projection)
 
 Promise.all([
@@ -32,7 +30,7 @@ Promise.all([
 function ready([json, incarceration, allPoints]) {
   let states = topojson.feature(json, json.objects.states)
   states.features = states.features.filter(d => d.id !== 72 && d.id !== 78)
-  var stateData = {}
+  let stateData = {}
   incarceration.forEach(function(d) {
     stateData[d.id] = d
   })
@@ -50,20 +48,51 @@ function ready([json, incarceration, allPoints]) {
     .append('path')
     .attr('class', 'state')
     .attr('d', path)
+    .attr('fill', 'rgba(0,0,0,0.01)')
+    .on('mouseenter', function(d) {
+      d3.select(this).attr('stroke', 'white')
+    })
+    .on('mouseleave', function(d) {
+      d3.select(this).attr('stroke', 'none')
+    })
 
-    /* Weird pudding stuff goes here */
-  var radiusScale = d3.scaleSqrt()
-    .domain([1000,50000])
-    .range([1,3.5])
+  /* Weird pudding stuff goes here */
+  let radiusScale = d3
+    .scaleSqrt()
+    .domain([1000, 50000])
+    .range([1, 3.5])
     .clamp(true)
 
-  var xExtent = d3.extent(allPoints, d => +d.x)
-  var yExtent = d3.extent(allPoints, d => +d.y)
+  let xExtent = d3.extent(allPoints, d => +d.x)
+  let yExtent = d3.extent(allPoints, d => +d.y)
 
-  let jailExtent = d3.extent(allPoints, d => d.jail_black_2010/d.total_2010)
+  let jailExtent = d3.extent(allPoints, d => d.jail_black_2010 / d.total_2010)
 
-  var colorScale = d3.scaleSequential(d3.interpolatePlasma)
+  let colorScale = d3
+    .scaleSequential(d3.interpolatePlasma)
     .domain(jailExtent)
     .clamp(true)
 
+  let xPositionScale = d3
+    .scaleLinear()
+    .domain(xExtent)
+    .range([0, width])
+  let yPositionScale = d3
+    .scaleLinear()
+    .domain(yExtent)
+    .range([height, 0])
+
+  svg
+    .append('g')
+    .lower()
+    .selectAll('circle')
+    .data(allPoints)
+    .enter()
+    .append('circle')
+    .attr('r', 2)
+    .attr('cx', d => xPositionScale(d.x))
+    .attr('cy', d => yPositionScale(d.y))
+    .attr('fill', d => {
+      return colorScale((d.jail_black_2010 / d.total_2010) * 50)
+    })
 }
--- src/07-map.js
+++ src/07-map.js
@@ -1,10 +1,8 @@
 import * as d3 from 'd3'
 import * as topojson from 'topojson'
 
-let margin = { top: 20, left: 20, right: 20, bottom: 20 }
-
+let margin = { top: 0, left: 0, right: 0, bottom: 0 }
 let height = 500 - margin.top - margin.bottom
-
 let width = 900 - margin.left - margin.right
 
 let svg = d3
@@ -13,15 +11,37 @@ let svg = d3
   .attr('height', height + margin.top + margin.bottom)
   .attr('width', width + margin.left + margin.right)
   .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+  .attr('transform', `translate(${margin.left},${margin.top})`)
+
+let projection = d3.geoMercator()
 
 let path = d3.geoPath().projection(projection)
 
-d3.json(require('./data/world.topojson'))
+let lineWidthScale = d3
+  .scaleLinear()
+  .domain([0, 100])
+  .range([0, 20])
+
+Promise.all([
+  d3.json(require('./data/world.topojson')),
+  d3.csv(require('./data/coordinates.csv')),
+  d3.csv(require('./data/transit-data.csv'))
+])
   .then(ready)
   .catch(err => console.log('Failed on', err))
 
-function ready(json) {
+let coordinateStore = d3.map()
+
+function ready([json, coordinateData, transitData]) {
+  // loop through the city/coord data
+  // and save the coords inside our
+  // coordinateStore
+  coordinateData.forEach(d => {
+    let name = d.name
+    let coords = [d.lon, d.lat]
+    coordinateStore.set(name, coords)
+  })
+
   let countries = topojson.feature(json, json.objects.countries)
   svg
     .append('g')
@@ -32,4 +52,38 @@ function ready(json) {
     .attr('class', 'country')
     .attr('d', path)
     .attr('fill', 'lightgrey')
+
+  // plotting cities on our map
+  svg
+    .selectAll('.city')
+    .data(coordinateData)
+    .enter()
+    .append('circle')
+    .attr('class', 'city')
+    .attr('r', 3)
+    .attr('transform', d => `translate(${projection([d.lon, d.lat])})`)
+
+  svg
+    .selectAll('.transit')
+    .data(transitData)
+    .enter()
+    .append('path')
+    .attr('d', d => {
+      // console.log(d.from)
+      // console.log(coordinateStore.get(d.from))
+      let fromCoords = coordinateStore.get(d.from)
+      let toCoords = coordinateStore.get(d.to)
+
+      let geoLine = {
+        type: 'LineString',
+        coordinates: [fromCoords, toCoords]
+      }
+
+      return path(geoLine)
+    })
+    .attr('fill', 'none')
+    .attr('stroke', 'red')
+    .attr('stroke-width', d => lineWidthScale(d.amount))
+    .attr('opacity', 0.5)
+    .attr('stroke-linecap', 'round')
 }
