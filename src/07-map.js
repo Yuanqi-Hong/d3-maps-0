5b3ab532e870e528bb98835ff1f0ce9e4ecd1847
Yuanqi (Edward) Hong <39491376+Yuanqi-Hong@users.noreply.github.com>
2018-10-10 10:18:14 -0400

    added classwork 10's add-on

--- src/07-map.js
+++ src/07-map.js
@@ -8,18 +8,12 @@ let height = 500 - margin.top - margin.bottom
 let width = 900 - margin.left - margin.right
 
 let svg = d3
-  .select('#chart-4')
+  .select('#chart-7')
   .append('svg')
   .attr('height', height + margin.top + margin.bottom)
   .attr('width', width + margin.left + margin.right)
   .append('g')
   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-
-let projection = d3.geoOrthographic()
-
-let nyc = [-74, 40]
-let london = [0, 51]
-let tehran = [51, 35]
 
 let path = d3.geoPath().projection(projection)
 
@@ -28,5 +22,14 @@ d3.json(require('./data/world.topojson'))
   .catch(err => console.log('Failed on', err))
 
 function ready(json) {
-
+  let countries = topojson.feature(json, json.objects.countries)
+  svg
+    .append('g')
+    .selectAll('.country')
+    .data(countries.features)
+    .enter()
+    .append('path')
+    .attr('class', 'country')
+    .attr('d', path)
+    .attr('fill', 'lightgrey')
 }
